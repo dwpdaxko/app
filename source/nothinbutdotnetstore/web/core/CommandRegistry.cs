d4a1aa5fd25c9413baa961735350233bab49c8a4
JimBruskalski <asmith@daxko.com>
2011-09-20 15:24:42 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -1,10 +1,25 @@
-﻿namespace nothinbutdotnetstore.web.core
+﻿using System.Collections.Generic;
+
+namespace nothinbutdotnetstore.web.core
 {
     public class CommandRegistry : IFindCommands
     {
+        private IEnumerable<IProcessOneRequest> processors;
+        public CommandRegistry(IEnumerable<IProcessOneRequest> processors)
+        {
+            this.processors = processors;
+        }
+
         public IProcessOneRequest get_the_command_that_can_process(IContainRequestInformation request)
         {
-            throw new System.NotImplementedException();
+            foreach(IProcessOneRequest processor in processors)
+            {
+                if(processor.can_process(request))
+                {
+                    return processor;
+                }
+            }
+            return new DoNotProcessRequest();
         }
     }
 }
---
+++ source/nothinbutdotnetstore/web/core/DoNotProcessRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetstore.web.core
+{
+    class DoNotProcessRequest : IProcessOneRequest
+    {
+        public void process(IContainRequestInformation request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool can_process(IContainRequestInformation request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
