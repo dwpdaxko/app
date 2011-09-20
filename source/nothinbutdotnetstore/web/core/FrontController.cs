80b0e1e647def9f3125ea3bdc0a29790ba09cab2
Patrick Boudreaux <pboudreaux@daxko.com>
2011-09-20 15:10:11 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -2,9 +2,17 @@
 {
     public class FrontController :IProcessRequests
     {
-        public void process(IContainRequestInformation request)
-        {
-            throw new System.NotImplementedException();
-        }
+    	private readonly IFindCommands _commandRegistry;
+
+    	public FrontController(IFindCommands commandRegistry)
+    	{
+    		_commandRegistry = commandRegistry;
+    	}
+
+    	public void process(IContainRequestInformation request)
+    	{
+    		var command = _commandRegistry.get_the_command_that_can_process(request);
+			command.process(request);
+    	}
     }
 }
