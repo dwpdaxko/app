e9d34f6f46fde79609cc8af40777f3ff73b082b7
Caleb Tucker <calebt@gmail.com>
2011-09-23 14:03:27 -0500

    Pushing new changes

--- source/nothinbutdotnetstore.specs/ViewTheDepartmentsInADepartmentRequestMapperSpecs.cs
+++ source/nothinbutdotnetstore.specs/ViewTheDepartmentsInADepartmentRequestMapperSpecs.cs
@@ -1,4 +1,7 @@
-﻿using developwithpassion.specifications.rhinomocks;
+﻿using System;
+using System.Collections.Specialized;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
 using nothinbutdotnetstore.web.application.catalogbrowsing;
 using nothinbutdotnetstore.web.core;
 
@@ -13,10 +16,30 @@ namespace nothinbutdotnetstore.specs
 
         public class when_mapping : concern
         {
-            public class and_all_the_information_is_there
+            public class and_all_the_information_is_there : concern
             {
-                public class and_it_is_valid
+                public class and_it_is_valid : concern
                 {
+                    Establish context = () =>
+                    {
+                        name_value_collection = new NameValueCollection();
+                        name_value_collection.Add("department_id", "1");
+                        name_value_collection.Add("introducted_into_store_on", "1/1/2001");
+                        name_value_collection.Add("number_of_items", "2");
+                    };
+
+                    Because b = () =>
+                        result = sut.map_from(name_value_collection);
+
+                    It should_return_a_request_with_the_correct_information = () =>
+                    {
+                        result.department_id.ShouldEqual(1);
+                        result.introduced_into_store_on.ShouldEqual(DateTime.Parse("1/1/2001"));
+                        result.number_of_items.ShouldEqual(2);
+                    };
+
+                    static NameValueCollection name_value_collection;
+                    static ViewTheDepartmentsOfADepartmentRequest result;
                 }
             }
         }
