786064cdbed8d26afd8adbc269dd7bc5923a11d9
swilliams <swilliams@daxko.com>
2011-09-23 18:33:30 -0500

    Pushing new changes

---
+++ source/nothinbutdotnetstore.specs/LinkBuildFactorySpecs.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using nothinbutdotnetstore.specs.utility;
+using nothinbutdotnetstore.web.core.link_builder;
+
+namespace nothinbutdotnetstore.specs
+{
+    public class LinkBuildFactorySpecs 
+    {
+         public abstract class concern : Observes<ICreateLinkBuilders, LinkBuilderFactory>
+         {
+             
+         }
+
+        public class when_asking_the_link_builder_factory_for_a_link_builder : concern
+        {
+            Establish context = () =>
+                {
+                    the_visitor = depends.@on<IProcessAToken>();
+                };
+            
+            Because b = () => result = sut.build_link(typeof(FakeType));
+
+            It should_retur_an_instance_of_the_link_builder =
+                () =>
+                    {
+                        var builder = result.ShouldBeAn<LinkBuilder>();
+                        builder.tokens.Count().ShouldEqual(1);
+                        builder.visitor.ShouldEqual(the_visitor);
+                    };
+
+            static IBuildLinks the_link_builder;
+            static IBuildLinks result;
+            static IProcessAToken the_visitor;
+        }
+    }
+}
---
+++ source/nothinbutdotnetstore/web/core/link_builder/LinkBuilderFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using nothinbutdotnetstore.utility.containers;
+
+namespace nothinbutdotnetstore.web.core.link_builder
+{
+    public class LinkBuilderFactory : ICreateLinkBuilders
+    {
+        readonly IProcessAToken visitor;
+
+        public LinkBuilderFactory(IProcessAToken visitor)
+        {
+            this.visitor = visitor;
+        }
+
+        public IBuildLinks build_link(Type request_type)
+        {
+            return new LinkBuilder(new TokenStore(), request_type, visitor);
+        }
+    }
+}
