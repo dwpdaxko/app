5309150add18d63e107408e73d93049ed9981fce
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-23 16:02:15 -0600

    Updated

--- source/nothinbutdotnetstore/web/core/stubs/StubSetOfCommands.cs
+++ source/nothinbutdotnetstore/web/core/stubs/StubSetOfCommands.cs
@@ -14,16 +14,15 @@ namespace nothinbutdotnetstore.web.core.stubs
 
         public IEnumerator<IProcessOneRequest> GetEnumerator()
         {
-            yield return
-                new RequestCommand(x => true,
-                                   Depends.on.a<QueryFor<IEnumerable<Department>, StubGetTheMainDepartments>>());
-            yield return
-                new RequestCommand(x => true,
-                                   Depends.on.a<QueryFor<IEnumerable<Department>, StubGetTheDepartmentsInADepartment>>())
-                ;
-            yield return
-                new RequestCommand(x => true,
-                                   Depends.on.a<QueryFor<IEnumerable<Product>, StubGetTheProductsInADepartment>>());
+            yield return create_command<IEnumerable<Department>,StubGetTheMainDepartments>();
+            yield return create_command<IEnumerable<Department>,StubGetTheDepartmentsInADepartment>();
+            yield return create_command<IEnumerable<Product>,StubGetTheProductsInADepartment>();
+        }
+
+        IProcessOneRequest create_command<ReportModel,QueryObject>() where QueryObject : IFetchA<ReportModel>
+        {
+           return new RequestCommand(x => true,
+               Depends.on.a<QueryFor<ReportModel,QueryObject>>>()) 
         }
     }
 }
