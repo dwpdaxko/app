210a2cb7ffe3134a97ed94007834d7b9ae2edf4f
Jace Bennett <jace.bennett@gmail.com>
2011-09-23 12:59:57 -0400

    Pushing new changes

---
+++ source/nothinbutdotnetstore.specs/RequestInformationSpecs.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Specialized;
+using System.Web;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs
+{
+	public class RequestInformationSpecs
+	{
+		public class concern : Observes<IContainRequestInformation, RequestInformation> {} 
+
+		public class when_mapping_an_input_model_that_needs_only_querystring_data : concern
+		{
+			Establish c = () =>
+			{
+				var actual_request = depends.on<HttpRequest>();
+				actual_request.setup(x => x.QueryString).Return(new NameValueCollection()
+				                                                {
+				                                                	{"id", "123"}
+				                                                });
+
+			};
+
+			Because b = () =>
+				result = sut.map_a<FakeInputModel>();
+
+			private It should_return_an_instance_of_specified_type_populated_with_data_from_request = () =>
+			{
+				result.id.ShouldEqual(123);
+			};
+
+			private static FakeInputModel result;
+		}
+
+		public class FakeInputModel
+		{
+			public int id { get; set; }
+		}
+	}
+
+}
---
+++ source/nothinbutdotnetstore/web/core/RequestInformation.cs
@@ -0,0 +1,10 @@
+﻿namespace nothinbutdotnetstore.web.core
+{
+	public class RequestInformation : IContainRequestInformation
+	{
+		public InputModel map_a<InputModel>()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
